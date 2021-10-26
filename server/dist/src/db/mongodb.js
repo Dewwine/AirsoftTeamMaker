72d63d3c0eb6b0898afbaa28ee9c3a6e211bc507
Dewwine <anton22.ab@gmail.com>
2021-10-26 10:57:50 +0300

    gitignore

--- server/dist/src/db/mongodb.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
-const mongoName = process.env.MONGO_NAME || '';
-const mongoUser = process.env.MONGODB_USER || '';
-const mongoPassword = process.env.MONGODB_PASSWORD || '';
-const mongoUrl = `mongodb+srv://${mongoUser}:${mongoPassword}@cluster0.zmkox.mongodb.net/${mongoName}?retryWrites=true&w=majority`;
-// const mongoUrl: string = `mongodb://mongo:27017/logSchema`;
-exports.default = mongoUrl;
